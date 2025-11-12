b148b5a5652851720a0502856774da31b140cf56
mussrabah <mustrbh@gmail.com>
2025-11-12 23:36:39 +0100

    Initial project

---
+++ src/main/kotlin/com/muss_coding/momentum_backend/MomentumBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.muss_coding.momentum_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MomentumBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<MomentumBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/muss_coding/momentum_backend/MomentumBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.muss_coding.momentum_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MomentumBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
